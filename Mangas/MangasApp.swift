5bda7d90991d28b25e7c16114ea590c6ab7f6425
Carlos Mata <carlosmata1009@gmail.com>
2024-08-12 16:25:19 +0200

    Initial Commit

---
+++ Mangas/MangasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangasApp.swift
+//  Mangas
+//
+//  Created by Carlos Mata on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MangasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
